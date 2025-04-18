c768431cd965d7c3b2b84fc6cd7e7bdb62f060eb
yuheswari v <yuheswariv22cs@psnacet.edu.in>
2025-04-18 08:27:25 +0000

    logical operators

---
+++ relationalop.java
@@ -0,0 +1,24 @@
+public class relationalop {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a!=b);
+
+
+        //logical operators
+
+        boolean c=true;
+        boolean d=false;
+        System.out.println(!c);                     //!NOT opearotrs
+        System.out.println(!d);
+        System.out.println(c||d);                     //||  OR  opearotrs  any one is T -> T
+        System.out.println(c&&d);                         //&& AND  ONLy if both Are T
+
+
+        //operator preceidance
+        int c=a*b+a;
+
+
+    }
+    
+}
