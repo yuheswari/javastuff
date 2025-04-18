f68fa3603a7e6d4fcf7dffe6deeaa6aea34daa8d
yuheswari v <yuheswariv22cs@psnacet.edu.in>
2025-04-18 08:05:09 +0000

    oprators

---
+++ operators.java
@@ -0,0 +1,32 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=10;
+        int b=4;
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        float f=(float)a/(float)b;  //type casting-->converting one data type to another datatype or implicit type conertion
+        //explicit we are changing manually
+        int g= a%b;
+        System.out.println(c);      
+        System.out.println(d);                            
+        System.out.println(e);                            
+        System.out.println(f);
+        System.out.println(g);                            
+                            
+                      
+                                                                            //binary opearotrs
+    
+
+    //unary operator
+    int h=5;
+    
+    System.out.println(--h);  //post increment   //prefix ++a
+    System.out.println(h);
+
+
+
+    
+
+}
+}
