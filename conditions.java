2d72b2a5fa40fc8596ef290be666ffe2524e170a
yuheswari v <yuheswariv22cs@psnacet.edu.in>
2025-04-18 13:39:35 +0000

    if

---
+++ conditions.java
@@ -0,0 +1,16 @@
+public class conditions {
+    public static void main(String[] args) {
+        char attendace ='a';
+        if(attendace=='p'){
+            System.out.println("SUCCESSFULLY DONE!!");
+        }else if(attendace=='o'){
+            System.out.println("SORRY onduty!!");
+        }
+        else{
+            System.out.println("not come!!");
+
+        }
+        
+    }
+    
+}
---
+++ nested.java
@@ -0,0 +1,22 @@
+public class nested {
+    public static void main(String[] args) {
+        char attendace='o';
+        int total=40;
+        if(attendace=='p'){
+            if(total >=50){
+                System.out.println("Classes can be attend!");
+            }}
+            else if(attendace=='o'){
+                if(total>=80){
+                    System.out.println("good");
+                }
+            }
+            else{
+                System.out.println("sorry");
+            }
+
+            System.out.println("program ends");
+        
+    }
+    
+}
