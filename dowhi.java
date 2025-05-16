18f86a2ba4023f5cf4f12cc99e1bae9a5f9ef436
yuheswari v <yuheswariv22cs@psnacet.edu.in>
2025-05-16 04:55:29 +0000

    java

---
+++ dowhi.java
@@ -0,0 +1,16 @@
+public class dowhi {
+    public static void main(String[] args) {
+        int n=5;
+        int sum=0;
+        int i=1;
+
+        do{
+            sum =sum+i;
+            i++;
+        }
+        while(i<=n);
+    }
+    System.out.println("sum:"+sum);
+
+    
+}
---
+++ natwhile.java
@@ -0,0 +1,16 @@
+public class natwhile {
+    public static void main(String[] args) {
+        int n=5;
+        int sum =0;
+        int i=0;
+
+        while (i<=n) {
+            sum+=i;
+            i++;
+            
+        }
+
+        System.out.println("ans:"+sum);
+    }
+    
+}
